d48638acc893f5e2d50806d5a35f3fe4bc663234
wastonsuen <sunzh95@hotmail.com>
2018-03-05 15:49:59 +0800

    coroutine

--- hello.py
+++ hello.py
@@ -7,15 +7,21 @@
 @time: 0:15
 @note:  ??
 """
-from typing import Iterable
-
 from tornado.web import RequestHandler, Application
 from tornado.ioloop import IOLoop
 
+import tornado.web
+import tornado.httpclient
+import tornado.escape
+
 
 class MainHandler(RequestHandler):
-    def get(self, *args, **kwargs):
-        self.write('Hello Tornado')
+    @tornado.web.gen.coroutine
+    def get(self):
+        http = tornado.httpclient.AsyncHTTPClient()
+        resp = yield http.fetch("https://dev.legendpoker.cn/weapp-redpacket/api/user/capital")
+        print(resp)
+        self.write(tornado.escape.json_decode(resp.body))
 
 
 class StoryHandler(RequestHandler):
@@ -27,10 +33,23 @@ class StoryHandler(RequestHandler):
             self.write('This is the story page: {}'.format(index))
 
 
+class PostHandler(RequestHandler):
+    def get(self, *args, **kwargs):
+        self.write('<html><body><form action="/message/" method="post">'
+                   '<input type="text" name="message">'
+                   '<input type="submit" value="Submit">'
+                   '</form></body></html>')
+
+    def post(self, *args, **kwargs):
+        self.set_header("Content-Type", "text/plain")
+        self.write("You wrote " + self.get_argument("message"))
+
+
 app = Application([
     (r'/', MainHandler),
-    (r'/story/([a-zA-Z0-9]*)', StoryHandler)
-])
+    (r'/story/(.*)', StoryHandler),
+    (r'/message/', PostHandler),
+], autoreload=True)
 
 if __name__ == '__main__':
     app.listen(8888)
